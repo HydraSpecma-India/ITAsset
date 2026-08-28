afbd6ac8131e19472ba6a2d2d2cb34ac07e4d38c
unknown <mani.sarathy121@gmail.com>
2026-08-28 10:48:36 +0530

    Import missing useCallback hook from react in components/Shell.js to fix build prerender error

--- components/Shell.js
+++ components/Shell.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import Link from "next/link";
 import { useAuth } from "@/lib/session";
